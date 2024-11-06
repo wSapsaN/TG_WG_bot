145cbf2e15a0038d1353f7dcdd7572efa318986b
nikolay <n.koshelev@tightvideo.com>
2024-11-06 14:09:52 +1000

    rename

--- bot/app.py
+++ bot/app.py
@@ -3,19 +3,29 @@ from aiogram import Bot, Dispatcher
 
 from admin import admin_route
 from client import client_route
-from middleware import SomeMidleware
+from middleware import DataBaseMD
 
 from config import TOKEN
+
+from database.engine import create_db, drop_db, session_maker
 
 bot = Bot(token=TOKEN)
 bot.admin_list = [667066036]
 
+async def start_up_bot():
+    print('\tConnection DataBase\t')
+    
+    await create_db()
+
 async def main():
+    
     dp = Dispatcher()
     dp.include_router(admin_route)
     dp.include_router(client_route)
 
-    admin_route.message.middleware(SomeMidleware())
+    dp.startup.register(start_up_bot)
+
+    admin_route.message.middleware(DataBaseMD(session_pool=session_maker))
 
     await dp.start_polling(bot)
 
