1e0df45850ba9a842db87ef77458ee161e4a6492
nikolay <n.koshelev@tightvideo.com>
2024-11-13 15:22:00 +1000

    rename sh file for next test

--- bot/genarate_wg.py
+++ bot/genarate_wg.py
@@ -1,4 +1,5 @@
 import os
 
 async def create_config(ip: str, user_name: str)-> None:
-    os.system(f"sudo ../wireguard_setup/creat_client.sh {user_name} {ip} 2>> error.txt")
+    # os.system(f"sudo ../wireguard_setup/creat_client.sh {user_name} {ip} 2>> error.txt")
+    os.system(f"sudo ../wireguard_setup/test_up.sh {user_name} {ip} 2>> error.txt")
