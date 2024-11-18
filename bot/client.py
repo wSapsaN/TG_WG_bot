a92ee72209e3e3a41c2d24138b00a2c2112b4b74
nikolay <n.koshelev@tightvideo.com>
2024-11-18 13:33:21 +1000

    add fuctional and instruction for user

--- bot/client.py
+++ bot/client.py
@@ -2,9 +2,9 @@ from aiogram.filters.command import Command, CommandStart
 from aiogram import Router, F, types
 from aiogram import Bot
 
-from keyboards import keyboard_client
-from inline_keyboards import inline
-from database.orm_clinet import add_client
+from keyboards.keyboards import keyboard_client
+from keyboards.inline_keyboards import req_vpn, instruction
+from database.orm_clinet import add_client, status_useVPN, up_request
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -18,17 +18,24 @@ async def cmd_st(message: types.Message):
 
 @client_route.message(F.text == 'VPN')
 async def vpn(message: types.Message, bot: Bot, session: AsyncSession):
-  await message.answer("Запрос на получение VPN отправлен. Ожидайте ответа")
-  await bot.send_message(
-    bot.admin_list[0],
-    f"Пользователь {message.from_user.full_name} @{message.from_user.username} {message.from_user.id} запрашивает доступ для VPN",
-    reply_markup=inline,
-  )
-
   await add_client(
     session=session, 
     id_telegram=message.from_user.id, 
     nik_name=message.from_user.username,
+  )
+
+  if await status_useVPN(session=session, id_telegram=message.from_user.id):
+    await message.answer("Вы уже отправили запрос, ожидайте ответа.")
+
+    return
+
+  await up_request(session=session, id_telegram=message.from_user.id)
+
+  await message.answer("Запрос на получение VPN отправлен. Ожидайте ответа")
+  await bot.send_message(
+    bot.admin_list[0],
+    f"Пользователь {message.from_user.full_name} @{message.from_user.username} {message.from_user.id} запрашивает доступ для VPN",
+    reply_markup=await req_vpn(),
   )
 
 # TODO: 
@@ -38,4 +45,32 @@ async def vpn(message: types.Message, bot: Bot, session: AsyncSession):
 
 @client_route.message(Command('help'))
 async def helper(message: types.Message):
-  await message.answer("Здесь должна быть помощь")
+  await message.answer("Здесь должна быть помощь", reply_markup=await instruction())
+
+@client_route.callback_query(F.data == 'windows')
+async def windows_instruction(callback: types.CallbackQuery):
+  await callback.answer('WINDOWS')
+  await callback.message.edit_text("Windows инструкция")
+
+@client_route.callback_query(F.data == 'android')
+async def android_instruction(callback: types.CallbackQuery):
+  await callback.answer('ANDROID')
+  await callback.message.edit_text("Android инструкция")
+
+
+@client_route.callback_query(F.data == 'ios')
+async def ios_instruction(callback: types.CallbackQuery):
+  await callback.answer('IOS')
+  await callback.message.edit_text(
+    """1. Скачайте приложение [WireGuard](https://apps.apple.com/ru/app/wireguard/id1441195209).
+2. *Нажмите на файл для просмотра*, который я отправил вам выше с расширением conf, нажмите на иконку для просмотра в правом нижнем углу, далее отправки из приложения в правом вехем углу телефона.
+3. Найдите приложение *WireGuard* и откройте его.
+4. В приложение *WireGuard* появиться строка с названием файла и кнопка для подключения к VPN'у.
+
+Приятного использования."""
+  )
+
+@client_route.callback_query(F.data == 'linux')
+async def linux_instruction(callback: types.CallbackQuery):
+  await callback.answer('LINUX')
+  await callback.message.edit_text("Linux инструкция")
--- bot/database/orm_clinet.py
+++ bot/database/orm_clinet.py
@@ -10,7 +10,6 @@ async def add_client(*, session: AsyncSession, id_telegram: int, nik_name: str):
     session.add(User(
         id_telegram=id_telegram,
         nik_name_telegram=nik_name,
-        reqests_vpn=True,
     ))
 
     await session.commit()
@@ -20,3 +19,31 @@ async def exist_user__(session: AsyncSession, id_telegram: int)-> bool:
     res = await session.execute(response)
     
     return bool(res.scalar())
+
+async def update_ip(
+        session: AsyncSession, 
+        id_telegram: int, 
+        address: str
+    )-> None:
+    
+    up_clinet = update(User).where(User.id_telegram == id_telegram).values(ip_client=address)
+    up_runtime = update(User).where(User.id_telegram == id_telegram).values(use_runtime=True)
+
+    await session.execute(up_clinet)
+    await session.execute(up_runtime)
+
+    await session.commit()
+
+async def status_useVPN(session: AsyncSession, id_telegram: int)-> int:
+
+    response = select(User.reqests_vpn).where(User.id_telegram == id_telegram)
+
+    result = await session.execute(response)
+
+    return int(result.scalar())
+
+async def up_request(session: AsyncSession, id_telegram: int)-> None:
+    response = update(User).where(User.id_telegram == id_telegram).values(reqests_vpn=1)
+
+    await session.execute(response)
+    await session.commit()
