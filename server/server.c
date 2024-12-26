78e15e6ee881d9365ae28d655112e5940e6df908
nikolay <n.koshelev@tightvideo.com>
2024-12-27 09:50:50 +1000

    server fix

--- server/server.c
+++ server/server.c
@@ -7,6 +7,67 @@
 
 #define PORT 8580
 
+char *pub_file(char *file_name)
+{
+  // Function for read pub clinet file
+  // end return charsets in head
+
+  char fname[100];
+
+  for (size_t i = 0; i < strlen(file_name); i++)
+  {
+    fname[i] = file_name[i];
+  }
+
+  char *pub = "_publickey";
+  strcat(fname, pub);
+
+  printf("%s\n", fname);
+
+  FILE *fp = fopen(fname, "r");
+
+  if (!fp)
+  {
+    printf("Can't open file with name %s\n", fname);
+    return NULL;
+  }
+
+  char *text = malloc(45);
+
+  fgets(text, 45, fp);
+
+  fclose(fp);
+
+  return text;
+}
+
+char *rname(char const *command)
+{
+  // returns the name from the command that comes into the buffer from the client
+
+  int count = 0, flag = 0, j = 0;
+  char *name_use = malloc(sizeof(command[0])*50);
+
+  while (1)
+  {
+    if (flag)
+    {
+      if (command[count] == ' '){
+        name_use[j] = '\0';
+        break;
+      }
+
+      name_use[j++] = command[count];
+    }
+
+    if (command[count] == ' ') flag = 1;
+
+    count++;
+  }
+
+  return name_use;
+}
+
 void server_up()
 {
   int server_fd, new_socket;
@@ -15,7 +76,7 @@ void server_up()
 
   socklen_t addrlen = sizeof(address);
   char buffer[1024] = { 0 };
-  char* hello = "Hello from server";
+  // char* hello = "Hello from server";
 
   // Creating socket file descriptor
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -36,13 +97,21 @@ void server_up()
 
     valread = read(new_socket, buffer, 1024 - 1);
     
-    printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-
-    char *cmd = buffer;
-
+    // in buffer = command <userName> <ip_address_user>
     system(buffer);
-    
+
+    // get use name for pubkey
+    char *names_use = rname(buffer);
+
+    // get publick key user
+    char *pubkey = pub_file(names_use);
+
+    free(names_use);
+    names_use = NULL;
+
+    // printf("%s\n", buffer);
+    send(new_socket, pubkey, strlen(pubkey), 0);
+   
     // closing the connected socket
     close(new_socket);
   }
