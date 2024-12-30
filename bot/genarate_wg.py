800f6eca5b90aa7ce6fe916a9b6c021fd961164d
nikolay <n.koshelev@tightvideo.com>
2024-12-30 14:21:18 +1000

    delete trash

--- bot/genarate_wg.py
+++
@@ -1,7 +0,0 @@
-import os
-from config import GENERATE_SCRIPT_path
-
-async def create_config(ip: str, user_name: str)-> str:
-    os.system(f"{GENERATE_SCRIPT_path} {user_name} {ip} 2>> error.txt")
-
-    return f"{user_name}.conf"
